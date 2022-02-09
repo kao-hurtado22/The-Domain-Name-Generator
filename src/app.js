f5897956bbf6e0bd03995ba08ab39bfa546fe1e8
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-02-09 18:52:25 +0000

    terminado domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let dominioIndex = Math.floor(Math.random() * dominio.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    adj[adjIndex] +
+    "" +
+    noun[nounIndex] +
+    "" +
+    dominio[dominioIndex]
+  );
 };
