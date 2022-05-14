5b97ec25377769f8382f27c47dd48b07e00c208b
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-05-14 01:27:24 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
-window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "awesome"];
+let noun = ["jogger", "racoon", "bear"];
+let dominio = [".com", ".cl", ".net"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dominio.length; l++)
+        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+    }
+  }
+}
+
+/* window.onload = function() {
   document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
@@ -7,7 +20,7 @@ let generateExcuse = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let dominio = [".com"];
+  let dominio = [".com", ".cl", ".net"];
 
   let pronounIndex = Math.floor(Math.random() * pronoun.length);
   let adjIndex = Math.floor(Math.random() * adj.length);
@@ -24,3 +37,4 @@ let generateExcuse = () => {
     dominio[dominioIndex]
   );
 };
+ */
